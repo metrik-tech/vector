ff21a3272a2ce75e9e48714a3e1ef7cae928686c
Erica Marigold <hi@devcomp.xyz>
2024-04-08 18:22:32 +0530

    feat: include HTTP server as interface for agent

--- src/agent.rs
+++ src/agent.rs
@@ -1,18 +1,19 @@
 use std::path::Path;
 
 use anyhow::Result;
+use async_std::{fs, task};
 use docker_api::{
     opts::{ContainerCreateOptsBuilder, ContainerRemoveOptsBuilder, PublishPort},
     Container, Docker, Id,
 };
 use once_cell::sync::Lazy;
+use rand::{distributions::Alphanumeric, Rng as _};
 use serde::{Deserialize, Serialize};
-use tokio::{fs, task};
 
-const LOCKFILE: Lazy<&Path> = Lazy::new(|| Path::new("deploy.lock"));
+pub const LOCKFILE: Lazy<&Path> = Lazy::new(|| Path::new("deploy.lock"));
 pub const DOCKER_UNIX_SOCK: &'static str = "/var/run/docker.sock";
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq)]
 pub enum ContainerStatus {
     Running,
     Deploying,
@@ -32,8 +33,8 @@ pub struct Agent {
 }
 
 impl Agent {
-    pub async fn new(docker_sock: String, image: String) -> Result<Self> {
-        let sock = Docker::unix(docker_sock);
+    pub async fn new<T: ToString>(docker_sock: T, image: T) -> Result<Self> {
+        let sock = Docker::unix(docker_sock.to_string());
         sock.ping().await?;
 
         Ok(Self {
@@ -41,10 +42,18 @@ impl Agent {
             container: sock
                 .containers()
                 .create(
-                    &ContainerCreateOptsBuilder::new("metrik-web")
-                        .image(image)
-                        .expose(PublishPort::tcp(8080), 8080)
-                        .build(),
+                    &ContainerCreateOptsBuilder::new(format!(
+                        "{}-{}",
+                        image.to_string(),
+                        rand::thread_rng()
+                            .sample_iter(&Alphanumeric)
+                            .take(7)
+                            .map(char::from)
+                            .collect::<String>()
+                    ))
+                    .image(image.to_string())
+                    .expose(PublishPort::tcp(8080), 8080)
+                    .build(),
                 )
                 .await?,
 
@@ -52,12 +61,18 @@ impl Agent {
         })
     }
 
-    pub async fn lock(&mut self) -> Result<()> {
+    pub async fn lock(&mut self) -> Result<&Self> {
         let lockfile = *LOCKFILE;
 
         if lockfile.exists() {
             let deserialized_lockfile =
                 serde_json::from_str::<AgentLockfile>(&fs::read_to_string(lockfile).await?)?;
+
+            if deserialized_lockfile.status == ContainerStatus::Deploying {
+                // TODO: cancel previous deploy first, then deploy freshly 
+                return Err(anyhow::Error::msg("agent is already deploying"));
+            }
+
             // FIXME: remove this clone
             self.old_container = Some(Container::new(
                 self.sock.clone(),
@@ -67,23 +82,34 @@ impl Agent {
 
         generate_lockfile(lockfile, self.container.id(), ContainerStatus::Deploying).await?;
 
-        Ok(())
+        Ok(&*self)
     }
 
     pub async fn deploy(mut self) -> Result<()> {
-        let old_container = self.old_container.take().ok_or(anyhow::Error::msg(
+        match self.old_container.take().ok_or(anyhow::Error::msg(
             "agent needs to be locked before deploying",
-        ))?;
+        )) {
+            Ok(container) => {
+                container
+                    .remove(
+                        &ContainerRemoveOptsBuilder::default()
+                            .volumes(false)
+                            .force(true)
+                            .link(false)
+                            .build(),
+                    )
+                    .await?;
 
-        old_container
-            .remove(
-                &ContainerRemoveOptsBuilder::default()
-                    .volumes(false)
-                    .force(true)
-                    .link(false)
-                    .build(),
-            )
-            .await?;
+                Ok::<_, anyhow::Error>(())
+            }
+            Err(err) => {
+                if LOCKFILE.exists() {
+                    return Err(err);
+                }
+
+                Ok(())
+            }
+        }?;
 
         task::spawn(async move {
             self.container.start().await?;
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,62 @@
-use constime::comptime;
+use std::net::SocketAddr;
+
+use agent::{Agent, DOCKER_UNIX_SOCK, LOCKFILE};
 use serde::Deserialize;
 use tide::{Request, Response, StatusCode};
-use agent::Agent;
-
-use crate::agent::DOCKER_UNIX_SOCK;
+use async_std::fs;
 
 mod agent;
 
 const DEPLOY_SECRET: &'static str = include_str!(concat!(env!("OUT_DIR"), "/secret.uuid"));
+const PORT: u16 = 33293;
 
 #[derive(Deserialize)]
 struct DeployEndpointBody {
     secret: String,
 }
 
-#[tokio::main]
+#[async_std::main]
 async fn main() {
     println!("DEPLOY_SECRET={}", DEPLOY_SECRET);
 
-    // let mut srv = tide::new();
+    let mut srv = tide::new();
 
-    // srv.at("/deploy").post(|mut req: Request<()>| async move {
-    //     let body =
-    //         req.body_json::<DeployEndpointBody>()
-    //             .await
-    //             .map_err(|mut err: tide::Error| {
-    //                 err.set_status(StatusCode::BadRequest);
-    //                 err
-    //             })?;
+    srv.at("/deploy").post(|mut req: Request<()>| async move {
+        let body =
+            req.body_json::<DeployEndpointBody>()
+                .await
+                .map_err(|mut err: tide::Error| {
+                    err.set_status(StatusCode::BadRequest);
+                    err
+                })?;
 
-    //     if body.secret != DEPLOY_SECRET {
-    //         return Err(tide::Error::new(
-    //             StatusCode::Unauthorized,
-    //             anyhow::Error::msg("invalid deploy secret"),
-    //         ));
-    //     }
+        if body.secret != DEPLOY_SECRET {
+            return Err(tide::Error::new(
+                StatusCode::Unauthorized,
+                anyhow::Error::msg("invalid deploy secret"),
+            ));
+        }
 
-    //     let mut resp = Response::new(StatusCode::Accepted);
-    //     resp.set_body(serde_json::json!({
-    //         "error": None::<()>,
-    //     }));
-
-    //     Ok(resp)
-    // });
-
-        let mut agent = Agent::new(DOCKER_UNIX_SOCK.to_string(), "hello-world".to_string()).await.unwrap();
+        let mut agent = Agent::new(DOCKER_UNIX_SOCK, "hello-world").await.unwrap();
         agent.lock().await.unwrap();
         agent.deploy().await.unwrap();
+
+        let resp = Response::new(StatusCode::Accepted);
+        Ok(resp)
+    });
+
+    srv.at("/status").get(|_| async {
+        Ok(serde_json::json!(fs::read_to_string(*LOCKFILE)
+            .await
+            .map_err(|err| tide::Error::new(
+                StatusCode::InternalServerError,
+                err
+            ))?))
+    });
+
+    println!("Listening on port {}", PORT);
+
+    srv.listen(SocketAddr::from(([127, 0, 0, 1], PORT)))
+        .await
+        .unwrap();
 }
