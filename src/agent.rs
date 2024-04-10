be9297b82daacee9b8607303d79b09cd34553bb2
Erica Marigold <hi@devcomp.xyz>
2024-04-10 12:22:09 +0530

    chore: tweak rustfmt rules

--- src/agent.rs
+++ src/agent.rs
@@ -3,8 +3,8 @@ use std::path::Path;
 use anyhow::Result;
 use async_std::{fs, stream::StreamExt as _, task};
 use docker_api::{
-    opts::{ContainerCreateOptsBuilder, ContainerRemoveOptsBuilder, PublishPort, PullOptsBuilder},
-    Container, Docker, Id,
+	opts::{ContainerCreateOptsBuilder, ContainerRemoveOptsBuilder, PublishPort, PullOptsBuilder},
+	Container, Docker, Id,
 };
 use log::{debug, trace, warn};
 use once_cell::sync::Lazy;
@@ -16,156 +16,139 @@ pub const DOCKER_UNIX_SOCK: &str = "/var/run/docker.sock";
 
 #[derive(Serialize, Deserialize, PartialEq)]
 pub enum ContainerStatus {
-    Running,
-    Deploying,
+	Running,
+	Deploying,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct AgentLockfile {
-    container_id: Id,
-    status: ContainerStatus,
+	container_id: Id,
+	status: ContainerStatus,
 }
 
 #[derive(Debug)]
 pub struct Agent {
-    sock: Docker,
-    container: Container,
-    old_container: Option<Container>,
+	sock: Docker,
+	container: Container,
+	old_container: Option<Container>,
 }
 
 impl Agent {
-    pub async fn new<T: ToString>(docker_sock: T, image: T) -> Result<Self> {
-        let sock = Docker::unix(docker_sock.to_string());
-        sock.ping().await?;
+	pub async fn new<T: ToString>(docker_sock: T, image: T) -> Result<Self> {
+		let sock = Docker::unix(docker_sock.to_string());
+		sock.ping().await?;
 
-        debug!("Connected to docker unix socket {}", DOCKER_UNIX_SOCK);
-        debug!("Pulling image {}", image.to_string());
+		debug!("Connected to docker unix socket {}", DOCKER_UNIX_SOCK);
+		debug!("Pulling image {}", image.to_string());
 
-        // TODO: pull from ghcr.io instead of docker hub
-        let local_images = sock.images();
-        let mut pull_stream = local_images.pull(
-            &PullOptsBuilder::default()
-                .image(image.to_string())
-                .tag("latest")
-                .build(),
-        );
+		// TODO: pull from ghcr.io instead of docker hub
+		let local_images = sock.images();
+		let mut pull_stream = local_images.pull(&PullOptsBuilder::default().image(image.to_string()).tag("latest").build());
 
-        while let Some(pull_res) = pull_stream.next().await {
-            let chunk = pull_res.map_err(anyhow::Error::from)?;
-            trace!("Pulling chunk: {:?}", chunk);
-        }
+		while let Some(pull_res) = pull_stream.next().await {
+			let chunk = pull_res.map_err(anyhow::Error::from)?;
+			trace!("Pulling chunk: {:?}", chunk);
+		}
 
-        let container_name = format!(
-            "{}-{}",
-            image.to_string(),
-            rand::thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(7)
-                .map(char::from)
-                .collect::<String>()
-        );
+		let container_name = format!(
+			"{}-{}",
+			image.to_string(),
+			rand::thread_rng().sample_iter(&Alphanumeric).take(7).map(char::from).collect::<String>()
+		);
 
-        debug!("Creating container {}", container_name);
+		debug!("Creating container {}", container_name);
 
-        Ok(Self {
-            sock: sock.clone(),
-            container: sock
-                .containers()
-                .create(
-                    &ContainerCreateOptsBuilder::new(container_name)
-                        .image(image.to_string())
-                        // FIXME: Actually get the ports right
-                        .expose(PublishPort::tcp(8080), 8080)
-                        .build(),
-                )
-                .await?,
+		Ok(Self {
+			sock: sock.clone(),
+			container: sock
+				.containers()
+				.create(
+					&ContainerCreateOptsBuilder::new(container_name)
+						.image(image.to_string())
+						// FIXME: Actually get the ports right
+						.expose(PublishPort::tcp(8080), 8080)
+						.build(),
+				)
+				.await?,
 
-            old_container: None,
-        })
-    }
+			old_container: None,
+		})
+	}
 
-    pub async fn lock(&mut self) -> Result<&Self> {
-        let lockfile = *LOCKFILE;
+	pub async fn lock(&mut self) -> Result<&Self> {
+		let lockfile = *LOCKFILE;
 
-        debug!("Locking {}", lockfile.display());
+		debug!("Locking {}", lockfile.display());
 
-        if lockfile.exists() {
-            let deserialized_lockfile =
-                serde_json::from_str::<AgentLockfile>(&fs::read_to_string(lockfile).await?)?;
+		if lockfile.exists() {
+			let deserialized_lockfile = serde_json::from_str::<AgentLockfile>(&fs::read_to_string(lockfile).await?)?;
 
-            if deserialized_lockfile.status == ContainerStatus::Deploying {
-                let old_container_id = deserialized_lockfile.container_id.clone();
-                warn!(
-                    "Previously abandoned deployment {} found, removing and redeploying",
-                    &old_container_id.to_string()
-                );
-            }
+			if deserialized_lockfile.status == ContainerStatus::Deploying {
+				let old_container_id = deserialized_lockfile.container_id.clone();
+				warn!(
+					"Previously abandoned deployment {} found, removing and redeploying",
+					&old_container_id.to_string()
+				);
+			}
 
-            // FIXME: remove this clone
-            self.old_container = Some(Container::new(
-                self.sock.clone(),
-                deserialized_lockfile.container_id,
-            ))
-        }
+			// FIXME: remove this clone
+			self.old_container = Some(Container::new(self.sock.clone(), deserialized_lockfile.container_id))
+		}
 
-        generate_lockfile(lockfile, self.container.id(), ContainerStatus::Deploying).await?;
+		generate_lockfile(lockfile, self.container.id(), ContainerStatus::Deploying).await?;
 
-        Ok(&*self)
-    }
+		Ok(&*self)
+	}
 
-    pub async fn deploy(mut self) -> Result<()> {
-        match self.old_container.take().ok_or(anyhow::Error::msg(
-            "Agent needs to be locked before deploying",
-        )) {
-            Ok(container) => {
-                fs::remove_file(*LOCKFILE).await?;
-                remove_container(container).await
-            }
-            Err(err) => {
-                if LOCKFILE.exists() {
-                    return Err(err);
-                }
+	pub async fn deploy(mut self) -> Result<()> {
+		match self
+			.old_container
+			.take()
+			.ok_or(anyhow::Error::msg("Agent needs to be locked before deploying"))
+		{
+			Ok(container) => {
+				fs::remove_file(*LOCKFILE).await?;
+				remove_container(container).await
+			}
+			Err(err) => {
+				if LOCKFILE.exists() {
+					return Err(err);
+				}
 
-                Ok(())
-            }
-        }?;
+				Ok(())
+			}
+		}?;
 
-        task::spawn(async move {
-            self.container.start().await?;
-            generate_lockfile(*LOCKFILE, self.container.id(), ContainerStatus::Running).await?;
+		task::spawn(async move {
+			self.container.start().await?;
+			generate_lockfile(*LOCKFILE, self.container.id(), ContainerStatus::Running).await?;
 
-            Ok::<(), anyhow::Error>(())
-        });
+			Ok::<(), anyhow::Error>(())
+		});
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 async fn remove_container(container: Container) -> Result<()> {
-    container
-        .remove(
-            &ContainerRemoveOptsBuilder::default()
-                .volumes(false)
-                .force(true)
-                .link(false)
-                .build(),
-        )
-        .await?;
+	container
+		.remove(&ContainerRemoveOptsBuilder::default().volumes(false).force(true).link(false).build())
+		.await?;
 
-    debug!("Removed container {}", container.id());
+	debug!("Removed container {}", container.id());
 
-    Ok(())
+	Ok(())
 }
 
 async fn generate_lockfile(lockfile: &Path, id: &Id, status: ContainerStatus) -> Result<()> {
-    debug!("Generating lockfile {}", lockfile.display());
+	debug!("Generating lockfile {}", lockfile.display());
 
-    let lockfile_contents = serde_json::to_string(&AgentLockfile {
-        container_id: id.clone(),
-        status,
-    })?;
+	let lockfile_contents = serde_json::to_string(&AgentLockfile {
+		container_id: id.clone(),
+		status,
+	})?;
 
-    fs::write(lockfile, lockfile_contents).await?;
+	fs::write(lockfile, lockfile_contents).await?;
 
-    Ok(())
+	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-#![allow(
-    clippy::borrow_interior_mutable_const,
-    clippy::declare_interior_mutable_const
-)]
+#![allow(clippy::borrow_interior_mutable_const, clippy::declare_interior_mutable_const)]
 
 use std::{env, net::SocketAddr, time::SystemTime};
 
@@ -20,87 +17,76 @@ const PORT: u16 = 33293;
 
 #[derive(Deserialize)]
 struct DeployEndpointBody {
-    secret: String,
+	secret: String,
 }
 
 #[async_std::main]
 async fn main() -> Result<()> {
-    fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{} {} {} {}",
-                humantime::format_rfc3339_seconds(SystemTime::now()).dimmed(),
-                match record.level() {
-                    log::Level::Error => "ERROR".red().to_string(),
-                    log::Level::Warn => "WARN".yellow().to_string(),
-                    log::Level::Info => "INFO".green().to_string(),
-                    log::Level::Debug => "DEBUG".cyan().to_string(),
-                    log::Level::Trace => "TRACE".blue().to_string(),
-                },
-                record.target().bold(),
-                message
-            ))
-        })
-        .level({
-            if cfg!(debug_assertions) {
-                log::LevelFilter::Trace
-            } else {
-                log::LevelFilter::Debug
-            }
-        })
-        .chain(std::io::stdout())
-        .chain(fern::log_file(format!(
-            "/tmp/{}_{}.log",
-            env::var("CARGO_PKG_NAME")?,
-            humantime::format_rfc3339(SystemTime::now())
-        ))?)
-        .apply()?;
+	fern::Dispatch::new()
+		.format(|out, message, record| {
+			out.finish(format_args!(
+				"{} {} {} {}",
+				humantime::format_rfc3339_seconds(SystemTime::now()).dimmed(),
+				match record.level() {
+					log::Level::Error => "ERROR".red().to_string(),
+					log::Level::Warn => "WARN".yellow().to_string(),
+					log::Level::Info => "INFO".green().to_string(),
+					log::Level::Debug => "DEBUG".cyan().to_string(),
+					log::Level::Trace => "TRACE".blue().to_string(),
+				},
+				record.target().bold(),
+				message
+			))
+		})
+		.level({
+			if cfg!(debug_assertions) {
+				log::LevelFilter::Trace
+			} else {
+				log::LevelFilter::Debug
+			}
+		})
+		.chain(std::io::stdout())
+		.chain(fern::log_file(format!(
+			"/tmp/{}_{}.log",
+			env::var("CARGO_PKG_NAME")?,
+			humantime::format_rfc3339(SystemTime::now())
+		))?)
+		.apply()?;
 
-    info!("DEPLOY_SECRET={}", DEPLOY_SECRET);
+	info!("DEPLOY_SECRET={}", DEPLOY_SECRET);
 
-    let mut srv = tide::new();
-    srv.with(tide::log::LogMiddleware::new());
+	let mut srv = tide::new();
+	srv.with(tide::log::LogMiddleware::new());
 
-    srv.at("/deploy").post(|mut req: Request<()>| async move {
-        let body =
-            req.body_json::<DeployEndpointBody>()
-                .await
-                .map_err(|mut err: tide::Error| {
-                    err.set_status(StatusCode::BadRequest);
-                    err
-                })?;
+	srv.at("/deploy").post(|mut req: Request<()>| async move {
+		let body = req.body_json::<DeployEndpointBody>().await.map_err(|mut err: tide::Error| {
+			err.set_status(StatusCode::BadRequest);
+			err
+		})?;
 
-        if body.secret != DEPLOY_SECRET {
-            return Err(tide::Error::new(
-                StatusCode::Unauthorized,
-                anyhow::Error::msg("invalid deploy secret"),
-            ));
-        }
+		if body.secret != DEPLOY_SECRET {
+			return Err(tide::Error::new(StatusCode::Unauthorized, anyhow::Error::msg("invalid deploy secret")));
+		}
 
-        fn handle_agent_error(err: anyhow::Error) -> tide::Error {
-            tide::Error::new(StatusCode::InternalServerError, err)
-        }
+		fn handle_agent_error(err: anyhow::Error) -> tide::Error {
+			tide::Error::new(StatusCode::InternalServerError, err)
+		}
 
-        let mut agent = Agent::new(DOCKER_UNIX_SOCK, "hello-world")
-            .await
-            .map_err(handle_agent_error)?;
-        agent.lock().await.map_err(handle_agent_error)?;
-        agent.deploy().await.map_err(handle_agent_error)?;
+		let mut agent = Agent::new(DOCKER_UNIX_SOCK, "hello-world").await.map_err(handle_agent_error)?;
+		agent.lock().await.map_err(handle_agent_error)?;
+		agent.deploy().await.map_err(handle_agent_error)?;
 
-        let resp = Response::new(StatusCode::Accepted);
-        Ok(resp)
-    });
+		let resp = Response::new(StatusCode::Accepted);
+		Ok(resp)
+	});
 
-    srv.at("/status").get(|_| async {
-        Ok(serde_json::json!(fs::read_to_string(*LOCKFILE)
-            .await
-            .map_err(|err| tide::Error::new(
-                StatusCode::InternalServerError,
-                err
-            ))?))
-    });
+	srv.at("/status").get(|_| async {
+		Ok(serde_json::json!(fs::read_to_string(*LOCKFILE)
+			.await
+			.map_err(|err| tide::Error::new(StatusCode::InternalServerError, err))?))
+	});
 
-    srv.listen(SocketAddr::from(([127, 0, 0, 1], PORT))).await?;
+	srv.listen(SocketAddr::from(([127, 0, 0, 1], PORT))).await?;
 
-    Ok(())
+	Ok(())
 }
